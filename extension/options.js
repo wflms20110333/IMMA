20dbe2947224a5647aff405dad8c84118ee4f7e1
Michelle Y <myakubek@outlook.com>
2020-06-29 10:38:35 -0500

    Adding option functions & webpage skeletons

--- extension/options.js
+++ extension/options.js
@@ -1,38 +1,45 @@
+// Manage the message frequency slider
+var freqslider = document.getElementById('frequency-slider');
+var slidertext = document.getElementById('slider-text');
+updateMessageFrequency(freqslider.value, slidertext);
+freqslider.oninput = function() {
+    updateMessageFrequency(freqslider.value, slidertext);
+};
+
+// Manage the fade checkbox
+var fadeswitch = document.getElementById('autofade-switch');
+fadeswitch.checked = true; // active by default
+fadeswitch.addEventListener('click', function() {
+    updateAutoDelete(fadeswitch.checked);
+});
+
 /**
  * Update the user's preferred message frequency
- * @param {number} xScale x-position of slider, between 0 and 10
+ * @param {number} xScale x-position of slider, between 0 and 20
+ * @param {string} sliderText text alongside the slider to label with amount
  */
-function updateMessageFrequency(xScale) {
+function updateMessageFrequency(xScale, sliderText) {
     console.log('in updateMessageFrequency');
 
     // First third of the slider: 5 seconds to 60 seconds
     // Second third of the slider: 1 minute to 5 minutes
     // Last third of the slider: 5 minutes to 20 minutes
 
-    var intScale = Math.ceil(xScale);
+    var intScale = Math.ceil(xScale/2);
     var answer = null;
     switch (intScale) {
-        case 1:
-            answer = [7, "every 5-10 seconds"];
-        case 2:
-            answer = [12, "every 10-15 seconds"];
-        case 3:
-            answer = [17, "every 15-20 seconds"];
-        case 4:
-            answer = [22, "every 20-30 seconds"];
-        case 5:
-            answer = [32, "every 30-45 seconds"];
-        case 6:
-            answer = [60, "every ~1 minute"];
-        case 7:
-            answer = [120, "every ~2 minutes"];
-        case 8:
-            answer = [300, "every ~5 minutes"];
-        case 9:
-            answer = [600, "every ~10 minutes"];
-        case 10:
-            answer = [1200, "every ~20 minutes"];
+        case 1:answer = [7, "every 10-15 seconds"];break;
+        case 2:answer = [12, "every 15-30 seconds"];break;
+        case 3:answer = [17, "every 30-45 seconds"];break;
+        case 4:answer = [22, "every 45-60 seconds"];break;
+        case 5:answer = [32, "every ~2 minutes"];break;
+        case 6:answer = [60, "every ~5 minutes"];break;
+        case 7:answer = [120, "every ~10 minutes"];break;
+        case 8:answer = [300, "every ~15 minutes"];break;
+        case 9:answer = [600, "every ~30 minutes"];break;
+        case 10:answer = [1200, "every ~1 hour"];break;
     }
+    sliderText.textContent = answer[1];
 
     // #TODO Update usersettings file to have this message frequency
 }
