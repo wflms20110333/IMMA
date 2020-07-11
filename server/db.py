7322fce38515aae20dc199ff37f9ae2873b0cb38
Elizabeth Zou <ezou@mit.edu>
2020-07-10 22:12:07 -0500

    Removed site tracking from database/server-side

--- server/db.py
+++ server/db.py
@@ -14,8 +14,7 @@ meta = MetaData(db)
 user_settings_table = Table('user_settings', meta,
                             Column('email', String),
                             Column('notif_frequency', Integer),
-                            Column('notif_fade', Boolean),
-                            Column('tracked_sites', ARRAY(String)))
+                            Column('notif_fade', Boolean))
 
 def execute_statement(statement):
     """ Executes an sqlalchemy statement on the database. """
@@ -30,23 +29,17 @@ def read_table(table):
     for r in result_set:
         print(r)
 
-def insert_user(email, notif_frequency, notif_fade, tracked_sites):
+def insert_user(email, notif_frequency, notif_fade):
     """ Inserts a user into the user_settings table. """
     insert_statement = user_settings_table.insert().values(email=email, 
                                                            notif_frequency=notif_frequency, 
-                                                           notif_fade=notif_fade, 
-                                                           tracked_sites=tracked_sites)
+                                                           notif_fade=notif_fade)
     execute_statement(insert_statement)
 
 def select_user(email):
     """ Returns the row proxy from selecting a row corresponding to a user. """
     select_statement = user_settings_table.select().where(user_settings_table.c.email == email)
     return execute_statement(select_statement).first()
-
-def get_tracked_sites(email):
-    """ Returns the list of tracked sites for a user. """
-    row_proxy = select_user(email)
-    return row_proxy["tracked_sites"]
 
 def update_notif_frequency(email, new_notif_frequency):
     """ Updates the notification frequency in seconds for a user. """
@@ -57,22 +50,6 @@ def update_notif_fade(email, new_notif_fade):
     """ Updates whether or not a user wants notifications to fade. """
     update_statement = user_settings_table.update().where(user_settings_table.c.email == email).values(notif_fade=new_notif_fade)
     execute_statement(update_statement)
-
-def add_tracked_site(email, site_to_add):
-    """ Adds a new site to track for a user, if it is not already tracked. """
-    tracked_sites = get_tracked_sites(email)
-    if site_to_add not in tracked_sites:
-        tracked_sites.append(site_to_add)
-        update_statement = user_settings_table.update().where(user_settings_table.c.email == email).values(tracked_sites=tracked_sites)
-        execute_statement(update_statement)
-
-def remove_tracked_site(email, site_to_remove):
-    """ Removes a site from being tracked for a user, if it is currently tracked. """
-    tracked_sites = get_tracked_sites(email)
-    if site_to_remove in tracked_sites:
-        tracked_sites.remove(site_to_remove)
-        update_statement = user_settings_table.update().where(user_settings_table.c.email == email).values(tracked_sites=tracked_sites)
-        execute_statement(update_statement)
 
 def delete_user(email):
     """ Deletes a user from the database. """
