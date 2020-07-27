3ade235c721372e2488024ccefe726c6e27b0aee
Michelle Y <myakubek@outlook.com>
2020-07-27 17:39:40 -0500

    add legal blurb to code

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,10 @@
-// Things to do at the beginning of code
+/* Copyright (C) 2020- IMMA Studio, LLC - All Rights Reserved
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ * You may not distribute, reproduce, or modify this code without written permission.
+ */
+
+ // Things to do at the beginning of code
 chrome.runtime.onInstalled.addListener(function () {
     // open startup page
     window.open("/welcome.html");
--- server/static/js/export.js
+++ server/static/js/export.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2020- IMMA Studio, LLC - All Rights Reserved
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ * You may not distribute, reproduce, or modify this code without written permission.
+ */
+
 // Put own URL in text area
 document.getElementById("selfLink").textContent = document.URL;
 
