1677cf0f56274563995a5534abfe1dace764117b
Elizabeth Zou <ezou@mit.edu>
2020-08-05 02:52:21 -0500

    db.py functions to insert/remove redeem codes

--- server/db.py
+++ server/db.py
@@ -1,5 +1,8 @@
+from sqlalchemy import and_
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, String, Integer, Boolean, ARRAY, MetaData
+from sqlalchemy import Table, Column, String, Integer, Boolean, MetaData #, ARRAY
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import exists
 
 username = 'postgres'
 password = 'imma-postgres'
@@ -11,10 +14,19 @@ db_string = 'postgres://' + username + ':' + password + '@' + endpoint + ':' + p
 db = create_engine(db_string)
 meta = MetaData(db)
 
+# create a configured "Session" class
+Session = sessionmaker(bind=db)
+# create a Session
+session = Session()
+
 user_settings_table = Table('user_settings', meta,
                             Column('uid', String),
                             Column('notif_frequency', Integer),
                             Column('notif_fade', Boolean))
+
+redeem_codes_table = Table('redeem_codes', meta,
+                           Column('uid', String),
+                           Column('code', String))
 
 def execute_statement(statement):
     """ Executes an sqlalchemy statement on the database. """
@@ -28,6 +40,22 @@ def read_table(table):
     result_set = execute_statement(select_statement)
     for r in result_set:
         print(r)
+
+def insert_code(uid, code):
+    """ Inserts a redeem code into the redeem_codes table. """
+    insert_statement = redeem_codes_table.insert().values(uid=uid, code=code)
+    execute_statement(insert_statement)
+
+def code_exists(uid, code):
+    """ Returns whether or not a given code exists. """
+    return session.query(exists().where(and_(redeem_codes_table.c.uid == uid,
+                                             redeem_codes_table.c.code == code
+                                        ))).scalar() is not None
+
+def remove_code(uid, code):
+    delete_statement = redeem_codes_table.delete().where(and_(redeem_codes_table.c.uid == uid,
+                                                              redeem_codes_table.c.code == code))
+    execute_statement(delete_statement)
 
 def insert_user(uid, notif_frequency, notif_fade):
     """ Inserts a user into the user_settings table. """
@@ -55,3 +83,16 @@ def delete_user(uid):
     """ Deletes a user from the database. """
     delete_statement = user_settings_table.delete().where(user_settings_table.c.uid == uid)
     execute_statement(delete_statement)
+
+
+print(code_exists("uid", "1234"))
+read_table(redeem_codes_table)
+insert_code("uid", "1234")
+print(code_exists("uid", "1234"))
+read_table(redeem_codes_table)
+insert_code("uid", "1234")
+print(code_exists("uid", "1234"))
+read_table(redeem_codes_table)
+remove_code("uid", "1234")
+print(code_exists("uid", "1234"))
+read_table(redeem_codes_table)
