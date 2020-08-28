95a6cb0a4c6e4032b286a03d0b33c76b6db4d663
Ayana Griffin <ayanajg@gmail.com>
2020-08-28 12:39:50 -0700

    minor positioning fixes

--- docs/browserbug/studio/studio.js
+++ docs/browserbug/studio/studio.js
@@ -224,15 +224,3 @@ $(document).ready(function() {
 		});
 	});
 });
-
-// function forwardOneScreen(curScreen){
-
-// 	if (curScreen === "name") {
-// 		document.getElementById("leftCol").style.display = "inline-block";
-// 		document.getElementById("leftCol").style.margin = "auto";
-// 		document.getElementById("centerCol").style.display = "none";
-// 		document.getElementById("right-arrow1").style.display = "inline-block";
-// 	}
-	
-	
-// }
