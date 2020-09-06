a3be83df9dc5d481fa299bb62f82f5e726d8fa70
Ayana Griffin <ayanajg@gmail.com>
2020-09-06 16:02:33 -0700

    added website folder

---
+++ docs/website/nav.js
@@ -0,0 +1,32 @@
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-content');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const btn = document.querySelector('.btn')
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation ='';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .3}s`
+            }  
+                
+        });
+
+        if(btn.style.animation){
+            btn.style.animation='';
+        }else{
+            btn.style.animation = `navLinkFade 0.5s ease forwards 1s`;
+        }
+        
+
+        burger.classList.toggle('toggle');
+    });
+
+    
+}
+
+navSlide();
